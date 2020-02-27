bf2ed3e7945447eb0e3c0b09bb5f59d89b3b5af2
danzjamz <djl.hovley@gmail.com>
2020-02-26 22:51:54 -0700

    nav burger toggle active class

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -6,13 +6,27 @@ import { NavLink } from 'react-router-dom';
 export default class Navigation extends Component {
     constructor(props) {
         super();
+
+        this.state = {
+            isToggle: false,
+            burgerActiveClass: ''
+        }
+    }
+
+    toggleBurger = () => {
+        console.log('toggleburger')
+        if (this.state.isToggle) {
+            this.setState({ isToggle: false, burgerActiveClass: 'burger-active' })
+        } else {
+            this.setState({ isToggle: true, burgerActiveClass: '' })
+        }
     }
 
     render() {
         return (
             <div className='nav'>
                 <div className='nav-left'>
-                    <i className="fas fa-hamburger burger"></i>
+                    <i className={"fas fa-hamburger burger " + this.state.burgerActiveClass} onClick={() => this.toggleBurger()}></i>
                     <NavLink exact to='/' className="nav-link">Home</NavLink>
                     <NavLink to='/new-trivia'className="nav-link">New Trivia</NavLink>
 
