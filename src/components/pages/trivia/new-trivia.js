e92ff823be2053109b2b76e4a6c10015ced2808e
danzjamz <djl.hovley@gmail.com>
2020-03-04 15:59:50 -0700

    added change handler and fixed button issue

--- src/components/pages/trivia/new-trivia.js
+++ src/components/pages/trivia/new-trivia.js
@@ -12,13 +12,22 @@ export default class NewTrivia extends Component {
             title: '',
             description: '',
             questions: '',
-            isOpen: '',
-            shouldWait: ''
+            isOpen: false,
+            shouldWait: false
         }
     }
 
-    submitForm = () => {
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
 
+    handleCheck = (event) => {
+        this.setState({ [event.target.name]: event.target.checked });
+    }
+
+    submitForm = (event) => {
+        console.log('in submit')
+        event.preventDefault();
     }
 
     render() {
@@ -26,27 +35,28 @@ export default class NewTrivia extends Component {
             <div className='new-trivia-container'>
                 <h1 className='new-trivia-title'>New Trivia</h1>
                 <div className='new-trivia-wrapper'>
-                    {/* <form onSubmit={ this.submitForm }> */}
-                    <form>
+                    <form onSubmit={ this.submitForm }>
+                    {/* <form> */}
                         <div>
                             <label>Title</label>
-                            <input className='form-el' type="text" name="title" placeholder="title"></input>
+                            <input className='form-el' type="text" name="title" placeholder="title" onChange={ () => this.handleChange(event) }></input>
                             <label>Description</label>
-                            <textarea className='form-el' type="text" name="description" placeholder="description" rows='10'></textarea>
+                            <textarea className='form-el' type="text" name="description" placeholder="description" rows='10' onChange={ () => this.handleChange(event) }></textarea>
                         </div>
                         <div>
                             <label>Trivia Settings</label>
                             <label className='form-el'>
-                                <input type="checkbox" name='isOpen'></input>
+                                <input type="checkbox" name='isOpen' onChange={ () => this.handleCheck(event) }></input>
                                 Open to the public
                             </label>
                             <label className='form-el'>
-                                <input type="checkbox" name='shouldWait'></input>
+                                <input type="checkbox" name='shouldWait' value='true' onChange={ () => this.handleCheck(event) }></input>
                                 Wait for players
                             </label>
                         </div>
-                        <div className="continue-btn form-el">
-                            <Link to='/new-trivia/questions'>Continue</Link>
+                        <div>
+                            {/* <Link to='/new-trivia/questions' type='submit'>Continue</Link> */}
+                            <button type='submit' className="continue-btn form-el">Continue</button>
                         </div>
                     </form>
                 </div>
