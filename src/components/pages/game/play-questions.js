fc7259a793a9372427d1752992693eb27849ba6f
danzjamz <djl.hovley@gmail.com>
2020-03-13 10:52:45 -0600

    player answers get passed to the results along with question and correct answers

--- src/components/pages/game/play-questions.js
+++ src/components/pages/game/play-questions.js
@@ -6,10 +6,12 @@ export default class PlayQuestions extends Component {
 
         this.state = {
             questions: this.props.location.state.questions,
-            currentQuestionIndex: 0
+            currentQuestionIndex: 0,
+            answerChosen: { },
+            answersChosen: [ ]
         }
 
-        console.log(props)
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // componentDidMount() {
@@ -24,21 +26,44 @@ export default class PlayQuestions extends Component {
         return currentQuestion.answers.map(answer => {
             return (
                 <li key={ answer.id }>
-                    <p>{ answer.answer }</p>
+                    <p onClick={ () => this.setAnswer(answer) }>{ answer.answer }</p>
                 </li>
             )
         })
     }
 
-    handleSubmit = (event) => {
+    setAnswer = (answer) => {
+        this.setState({ answerChosen: answer });
+        console.log(this.state.answerChosen)
+    }
+
+    async handleSubmit()  {
+        const currentQuestion = this.state.questions[this.state.currentQuestionIndex];
+        const correctAnswers = currentQuestion.answers.filter(answer => {
+            return answer.is_correct_answer === true;
+        });
+
+        await this.setState({
+            answersChosen: [ ...this.state.answersChosen, 
+                { 
+                    question: currentQuestion.question,
+                    correctAnswers: correctAnswers,
+                    answerChosen: this.state.answerChosen 
+                }
+            ]
+        });
+
         if (this.state.currentQuestionIndex < this.state.questions.length - 1) {
-            this.setState({ currentQuestionIndex: this.state.currentQuestionIndex + 1 });
+            this.setState({ 
+                currentQuestionIndex: this.state.currentQuestionIndex + 1 
+            });
         } else {
+            console.log(this.state.answersChosen);
             const triviaId = this.state.questions[this.state.currentQuestionIndex].trivia_id;
-            this.props.history.push(`/trivia/${triviaId}/play/results`);
+            this.props.history.push(`/trivia/${triviaId}/play/results`, { answersChosen: this.state.answersChosen });
         }
 
-        event.preventDefault();
+        // event.preventDefault();  // down here triggers warning, at top doesn't; seems to work without it at
     }
 
     render() {
--- src/components/pages/game/results.js
+++ src/components/pages/game/results.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-export default function Results() {
-    
+export default function Results(props) {
+    const playerAnswers = props.location.state.answersChosen;
+    console.log(playerAnswers)
+
+    const renderAnswers = (correctAnswers) => {
+        let isCorrect = false;
+        return correctAnswers.map(correctAnswer => {
+            // if (playerAnswers.answerChosen.id === correctAnswer.id) {
+            //     isCorrect = true;
+            // }
+            return <div key={ correctAnswer.id }>Correct Answer: { correctAnswer.answer }</div>
+        });
+    }
 
     return (
         <div>
-            Results
+            { playerAnswers ? (
+                playerAnswers.map(answerData => {
+                    return (
+                        <div key={ answerData.answerChosen.id }>
+                            <div>Question: { answerData.question }</div>
+                            <div>Your Answer: { answerData.answerChosen.answer }</div>
+                            { renderAnswers(answerData.correctAnswers) }
+                        </div>
+                    )
+                })
+            ) : null }
         </div>
     )
 }
