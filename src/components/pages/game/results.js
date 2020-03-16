c40ae1684185b2ba85d6a352f36e4525266a69d9
danzjamz <djl.hovley@gmail.com>
2020-03-16 12:34:02 -0600

    results page renders specific data to player based if they have a correct answer

--- src/components/pages/game/results.js
+++ src/components/pages/game/results.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 export default function Results(props) {
     const playerAnswers = props.location.state.answersChosen;
-    console.log(playerAnswers)
 
-    const renderAnswers = (correctAnswers) => {
+    const renderCorrectAnswers = (chosenAnswer, correctAnswers) => {
         let isCorrect = false;
-        return correctAnswers.map(correctAnswer => {
-            // if (playerAnswers.answerChosen.id === correctAnswer.id) {
-            //     isCorrect = true;
-            // }
-            return <div key={ correctAnswer.id }>Correct Answer: { correctAnswer.answer }</div>
+        let otherCorrectAnswers = [ ];
+
+        return correctAnswers.map((correctAnswer, index) => {
+            if (chosenAnswer.id === correctAnswer.id) {
+                isCorrect = true;  
+            } else if (isCorrect === true && index > 0) {
+                otherCorrectAnswers.push(correctAnswer);
+            }
+            
+            if (otherCorrectAnswers.length === 0 && isCorrect) {
+                return <div key='chosenAnswer.id'>{ chosenAnswer.answer } is Correct! </div>
+            } else if (otherCorrectAnswers.length > 1) {
+                return (
+                    <div>
+                        Other correct answers: { otherCorrectAnswers.map(ans => {
+                            return <div key='ans.id'> {ans.answer} </div>;
+                        })}
+                    </div>
+                )
+            } else {
+                return <div key={ correctAnswer.id }>Another correct answer: { correctAnswer.answer }</div>
+            } // if (otherCorrectAnswers.length === 1) 
         });
     }
 
@@ -21,8 +37,8 @@ export default function Results(props) {
                     return (
                         <div key={ answerData.answerChosen.id }>
                             <div>Question: { answerData.question }</div>
-                            <div>Your Answer: { answerData.answerChosen.answer }</div>
-                            { renderAnswers(answerData.correctAnswers) }
+                            {/* <div>Your Answer: { answerData.answerChosen.answer }</div> */}
+                            { renderCorrectAnswers(answerData.answerChosen, answerData.correctAnswers) }
                         </div>
                     )
                 })
