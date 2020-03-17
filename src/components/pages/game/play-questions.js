7d01a86b6436b4c84015c0440cd20aec92d6611c
danzjamz <djl.hovley@gmail.com>
2020-03-16 23:01:26 -0600

    timer works!

--- src/components/pages/game/play-questions.js
+++ src/components/pages/game/play-questions.js
@@ -8,11 +8,22 @@ export default class PlayQuestions extends Component {
             questions: this.props.location.state.questions,
             currentQuestionIndex: 0,
             answerChosen: { },
-            answersChosen: [ ]
+            answersChosen: [ ],
+            timer: 0
         }
+        let intervalId = null;
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    componentDidMount() {
+        this.setState({ timer: this.state.questions[this.state.currentQuestionIndex].time });
+        this.setTimer();
+    }
+
+    //  componentWillUnmount() {
+    //     clearInterval(this.intervalId);
+    //  }
 
     // ADD CATEGORY TO THE TOP OF THE QUESTION and timer
 
@@ -65,12 +76,36 @@ export default class PlayQuestions extends Component {
         // event.preventDefault();  // down here triggers warning, at top doesn't; seems to work without it at
     }
 
+    setTimer = () => {
+        this.intervalId = setInterval(() => {
+            if (this.state.timer > 0) {
+                if (this.state.timer <= 10) {
+                    this.setState({ timer: `0${ this.state.timer - 1 }` });
+                } else {
+                    this.setState({ timer: this.state.timer - 1 });
+                }
+            }
+        }, 1000);
+
+        if (this.state.time === 0) {
+            clearInterval(this.intervalId);
+        }
+    }
+
     render() {
+        const index = this.state.currentQuestionIndex;
         return (
             <div className='play-container'>
                 <div className='play-wrapper'>
-                    <div className='play-item game-question'>
-                        { this.state.questions[this.state.currentQuestionIndex].question }
+                    <div className='game-question'>
+                        <h2>
+                            { this.state.questions[index].question }
+                        </h2>
+                        <div className={'timer ' + (this.state.timer === '00' ? 'no-time' : this.state.timer <= 5 ? 'low-time' : null) }>
+                            { this.state.questions[index].time > 0 ? (
+                                this.state.timer
+                            ) : null }
+                        </div>
                     </div>
                     
                     <ul className='play-item'>
@@ -78,7 +113,7 @@ export default class PlayQuestions extends Component {
                     </ul>
 
                     <div className='play-item button-div'>
-                        { this.state.currentQuestionIndex < this.state.questions.length - 1 ? (
+                        { index < this.state.questions.length - 1 ? (
                             <button className='game-btns' onClick={ this.handleSubmit }>Next Question</button>
                             ) : (
                             <button className='game-btns' onClick={ this.handleSubmit }>Finish</button>                
